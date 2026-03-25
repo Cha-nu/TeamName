2238d356b448f9d90b42dcd6590db06daa616a08
Cha-Nu <cksdn43@cau.ac.kr>
2026-03-25 21:55:05 +0900

    [init] 프로젝트의 간단한 구조화

--- TeamName/TeamName.cpp
+++ TeamName/TeamName.cpp
@@ -1,7 +1,17 @@
 
+// 게임 실행 파일입니다.
+
 #include <iostream>
+#include "Manager/GameManager/GameManager.h"
+
+using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     std::cout << "Hello World!\n";
+
+    return 0;
 }

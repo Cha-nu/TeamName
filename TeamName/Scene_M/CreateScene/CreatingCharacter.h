800a795c831d652d7ffb2f715c31596581c89893
MinSeong926 <kms20644@naver.com>
2026-03-26 16:35:40 +0900

    [v_03] 플레이어 생성 수정

---
+++ TeamName/Scene_M/CreateScene/CreatingCharacter.h
@@ -0,0 +1,17 @@
+﻿// 캐릭터 생성 화면.h
+
+#pragma once
+#include "Scene.h"
+
+// 실제 시작 화면 (자식 클래스)
+class CharacterChoice : public Scene {
+public:
+	CharacterChoice() {} // 생성자
+	~CharacterChoice() override{} // 부모 클래스의 가상 소멸자를 override
+
+	// 함수 재정의
+	void Init() override;
+	void Update() override;
+	void Render() override;
+	void Exit() override;
+};
---
+++ TeamName/Scene_M/EndScene/EndScene.h
@@ -0,0 +1,17 @@
+﻿// 종료 화면.h
+
+#pragma once
+#include "Scene.h"
+
+// 실제 종료 화면 (자식 클래스)
+class GameOverScene : public Scene {
+public:
+	GameOverScene(){} // 생성자
+	~GameOverScene() override{} // 부모 클래스의 가상 소멸자를 override
+
+	// 함수 재정의
+	void Init() override;
+	void Update() override;
+	void Render() override;
+	void Exit() override;
+};

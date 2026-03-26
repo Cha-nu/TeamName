4792b598f9a4b8e305ebaba61f974f0637e5b8c1
saka4528 <nozomu466@gmail.com>
2026-03-26 17:25:15 +0900

    NormalMonster 클래스 정

--- TeamName/Monster/MonsterTest.cpp
+++ TeamName/Monster/MonsterTest.cpp
@@ -1,21 +1,21 @@
 ﻿//Monster 디버그용
 
-#include <iostream>
-#include <string>
-#include "Monster.h"
-
-int main(void){
-    Monster* monster = new Monster();
-	Monster* monster2 = new Monster({ "abc", 10, 10});
-    std::cout << monster->getName() << std::endl;
-    std::cout << *monster << std::endl;
-    std::cout << *monster2 << std::endl;
-    while (!monster2->isDead()) {
-        monster2->takeDamage(1);
-    }
-
-    delete monster;
-    delete monster2;
-
-    return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include "Monster.h"
+//
+//int main(void){
+//    NormalMonster* monster = new NormalMonster();
+//	NormalMonster* monster2 = new NormalMonster({ "abc", 10, 10});
+//    std::cout << monster->getName() << std::endl;
+//    std::cout << *monster << std::endl;
+//    std::cout << *monster2 << std::endl;
+//    while (!monster2->isDead()) {
+//        monster2->takeDamage(1);
+//    }
+//
+//    delete monster;
+//    delete monster2;
+//
+//    return 0;
+//}
--- TeamName/Monster/NormalMonster.cpp
+++ TeamName/Monster/NormalMonster.cpp
@@ -1,35 +1,30 @@
 ﻿#include "Monster.h"
 #include <iostream>
 
-//기본생성자
-Monster::Monster(){
-	InitializeStat();
-}
-
 //이름/체력/공격력/몬스터 타입 생성자
-Monster::Monster(MonsterStat stat){
+NormalMonster::NormalMonster(MonsterStat stat){
 	InitializeStat(stat);
 }
 
-void Monster::InitializeStat(MonsterStat stat) {
+void NormalMonster::InitializeStat(MonsterStat stat) {
 	this->stat = stat;
 }
 
 //name, hp, atk getter
-std::string Monster::getName() const{
+std::string NormalMonster::getName() const{
 	return this->stat.name;
 }
 
-int Monster::getHealth() const{
-	return this->stat.hp;
-}
-
-int Monster::getAttack() const{
+int NormalMonster::getAttack() const {
 	return this->stat.atk;
 }
 
+int NormalMonster::getHealth() const {
+	return this->stat.hp;
+}
+
 //피격 처리
-void Monster::takeDamage(int damage){
+void NormalMonster::takeDamage(int damage){
     this->stat.hp -= damage;
     if (this->isDead()) { // this->hp < 0
         std::cout << this->stat.name << " 사망" << std::endl; // 사망 로그
@@ -39,17 +34,17 @@ void Monster::takeDamage(int damage){
     }
 }
 
-void Monster::playerAttack() {}
+void NormalMonster::playerAttack() {}
 
 //사망 판정
-bool Monster::isDead() const{
+bool NormalMonster::isDead() const{
     return this->stat.hp < 0;
 }
 
 // 디버그용
 
-std::ostream& operator<<(std::ostream& os, const Monster& m)
+std::ostream& operator<<(std::ostream& os, const NormalMonster& m)
 {
-    os << "Monster 출력\nName: " << m.stat.name << ", HP: " << m.stat.hp << ", ATK: " << m.stat.atk;
+    os << "Monster 출력\nName: " << m.stat.name << ", HP: " << m.stat.hp << ", ATK: " << m.stat.atk << ", Type: " << typeid(m).name();
     return os;
 }

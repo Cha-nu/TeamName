c4a50940e0ad2699c2d131ff6b459958d197be4b
gimunkwon <gimon71@gmail.com>
2026-03-26 15:42:08 +0900

    [Refac] 생성자 기본값 초기화및 레벨추가
    생성자에서 플레이어의 레벨1,초기체력200,공격력30,경험치0으로 초기화
    
    경험치를 얻고 레벨을 올려주는 메서드 AcquireEXP 추가

--- TeamName/Player/Player.cpp
+++ TeamName/Player/Player.cpp
@@ -1,23 +1,41 @@
 #include "Player.h"
 
-Player::Player() : Playerstat(){
-    Playerstat.HP = 0;
-    Playerstat.name = "None";
-    Playerstat.Stamina = 0;
-    Playerstat.Gender = "Male";
+
+
+Player::Player(std::string name){
+	Playerstat.HP = 200;
+	Playerstat.Stamina = 30;
+	Playerstat.name = name;
+	Playerstat.Gender = "Male";
 	Playerstat.Atk_Damage = 0;
+	Playerstat.Level = 1;
+	Playerstat.EXP = 0;
 }
 
 Player::~Player(){
 }
 
-void Player::InitializeStat(int _hp, std::string _name, int _atkdamage, int _stamina){
+void Player::InitializeStat(int _hp, std::string _name, int _atkdamage,int _level,int _stamina){
 	Playerstat.HP = _hp;
 	Playerstat.name = _name;
 	Playerstat.Atk_Damage = _atkdamage;
+	Playerstat.Level = _level;
 	Playerstat.Stamina = _stamina;
 }
 
+
+void Player::AcquireEXP(int _exp)
+{
+	// if EXP amount over 100
+	// EXP set zero
+	// Level Plus 1
+	Playerstat.EXP += _exp;
+	if (Playerstat.EXP >= 100)
+	{
+		Playerstat.EXP = 0;
+		Playerstat.Level++;
+	}
+}
 
 void Player::Attack(){
     //Need to MonsterInterface

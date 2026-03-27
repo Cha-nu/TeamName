56a29ceaa17f5381bd2ca6710d2197b0d70be614
saka4528 <nozomu466@gmail.com>
2026-03-27 16:00:18 +0900

    주석 수

--- TeamName/Monster/BossMonster.cpp
+++ TeamName/Monster/BossMonster.cpp
@@ -1,25 +1,25 @@
-﻿//#include "BossMonster.h"
-//
-//BossMonster::BossMonster(MonsterStat stat) {
-//	InitializeStat(stat);
-//	initializeIntro();
-//}
-//
-////등장 대사 초기화 
-//void BossMonster::initializeIntro(std::string dialog) {
-//	this->dialogue = dialog;
-//}
-//
-////등장 대사 출력
-//void BossMonster::introMonster() {
-//	std::cout << this->dialogue << std::endl;
-//	std::cout << "보스 몬스터 " << this->stat.name << " 이 등장했습니다." << std::endl;
-//}
-//
-//// 디버그용
-//
-//std::ostream& operator<<(std::ostream& os , const BossMonster& m)
-//{
-//	os << "Monster 출력\nName: " << m.stat.name << ", HP: " << m.stat.hp << ", ATK: " << m.stat.atk << ", Type: " << typeid(m).name() << "\nIntro: " << m.dialogue;
-//	return os;
-//}
+﻿#include "BossMonster.h"
+
+BossMonster::BossMonster(MonsterStat stat) {
+	InitializeStat(stat);
+	initializeIntro();
+}
+
+//등장 대사 초기화 
+void BossMonster::initializeIntro(std::string dialog) {
+	this->dialogue = dialog;
+}
+
+//등장 대사 출력
+void BossMonster::introMonster() {
+	std::cout << this->dialogue << std::endl;
+	std::cout << "보스 몬스터 " << this->stat.name << " 이 등장했습니다." << std::endl;
+}
+
+// 디버그용
+
+std::ostream& operator<<(std::ostream& os , const BossMonster& m)
+{
+	os << "Monster 출력\nName: " << m.stat.name << ", HP: " << m.stat.hp << ", ATK: " << m.stat.atk << ", Type: " << typeid(m).name() << "\nIntro: " << m.dialogue;
+	return os;
+}
--- TeamName/Monster/MonsterTest.cpp
+++ TeamName/Monster/MonsterTest.cpp
@@ -1,35 +1,35 @@
 ﻿//Monster 디버그용
-#include <string>
-#include "Monster.h"
-#include "BossMonster.h"
-
-int main(void){
-	NormalMonster* monster2 = new NormalMonster();
-	BossMonster* monster3 = new BossMonster();
-	Player* player = new Player("abc");
-    std::cout << *monster2 << std::endl;
-	std::cout << *monster3 << std::endl;
-	std::cout << "\n\n\n" << std::endl;
-
-
-	monster2->introMonster();
-    while (!monster2->isDead()) {
-		monster2->attackPlayer(player);
-		player->ShowPlayerStat();
-		player->Attack(monster2);
-    }
-
-	std::cout << "\n\n\n" << std::endl;
-
-	monster3->introMonster();
-	while (!monster3->isDead()) {
-		monster3->attackPlayer(player);
-		player->ShowPlayerStat();
-		player->Attack(monster3);
-	}
-
-	delete player;
-    delete monster2;
-	delete monster3;
-    return 0;
-}
+//#include <string>
+//#include "Monster.h"
+//#include "BossMonster.h"
+//
+//int main(void){
+//	NormalMonster* monster2 = new NormalMonster();
+//	BossMonster* monster3 = new BossMonster();
+//	Player* player = new Player("abc");
+//    std::cout << *monster2 << std::endl;
+//	std::cout << *monster3 << std::endl;
+//	std::cout << "\n\n\n" << std::endl;
+//
+//
+//	monster2->introMonster();
+//    while (!monster2->isDead()) {
+//		monster2->attackPlayer(player);
+//		player->ShowPlayerStat();
+//		player->Attack(monster2);
+//    }
+//
+//	std::cout << "\n\n\n" << std::endl;
+//
+//	monster3->introMonster();
+//	while (!monster3->isDead()) {
+//		monster3->attackPlayer(player);
+//		player->ShowPlayerStat();
+//		player->Attack(monster3);
+//	}
+//
+//	delete player;
+//    delete monster2;
+//	delete monster3;
+//    return 0;
+//}

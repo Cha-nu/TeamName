95fd901cee4a21ea97348f6fbb361f0f48450e9c
sleeptired <107916334+sleeptired@users.noreply.github.com>
2026-03-27 11:18:21 +0900

    인벤토리 세팅

--- TeamName/Scene/InventoryScene.h
+++ TeamName/Scene/InventoryScene.h
@@ -1,11 +1,13 @@
 ﻿#include "Scene.h"
 
 class Player;
+class Inventory;
 
 class InventoryScene :public Scene
 {
 private:
 	Player* player;//
+	Inventory* player_Inventory;
 public:
 	void Init() override;
 	void Render() override;

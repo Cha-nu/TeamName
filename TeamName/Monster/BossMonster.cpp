4281983993eb695e4afc22f96728f11feb09ef81
saka4528 <nozomu466@gmail.com>
2026-03-27 14:12:19 +0900

    Monster 부모 클래스 구조 수정

---
+++ TeamName/Monster/BossMonster.cpp
@@ -0,0 +1,19 @@
+﻿#include "BossMonster.h"
+
+BossMonster::BossMonster() {
+	MonsterStat stat;
+	InitializeStat({ "Test Boss Mob", 200, 20, 100 }); // 이름 / 체력 / 공격력 / 잡았을 시 주는 경험치
+}
+
+BossMonster::BossMonster(MonsterStat stat) {
+	InitializeStat(stat);
+}
+
+
+// 디버그용
+
+std::ostream& operator<<(std::ostream& os , const BossMonster& m)
+{
+	os << "Monster 출력\nName: " << m.stat.name << ", HP: " << m.stat.hp << ", ATK: " << m.stat.atk << ", Type: " << typeid(m).name();
+	return os;
+}

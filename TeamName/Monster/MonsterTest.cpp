00996c09af07c1cc6e6bda032ec2d373131f203c
saka4528 <nozomu466@gmail.com>
2026-03-30 16:43:48 +0900

    몬스터 테스트 주석처

--- TeamName/Monster/MonsterTest.cpp
+++ TeamName/Monster/MonsterTest.cpp
@@ -1,33 +1,32 @@
 ﻿////Monster 디버그용
-#include <string>
-#include "Monster.h"
-#include "BossMonster.h"
+//#include <string>
+//#include "Monster.h"
+//#include "BossMonster.h"
+//int main(void) {
+//	NormalMonster* monster2 = new NormalMonster();
+//	monster2->getDropTableFromFile();
+//	monster2->getDroptable()->PrintItemList();
 //
-int main(void) {
-	NormalMonster* monster2 = new NormalMonster();
-	monster2->getDropTableFromFile();
-	monster2->getDroptable()->PrintItemList();
-
-	BossMonster* monster3 = new BossMonster();
-	monster3->getDropTableFromFile();
-	monster3->getDroptable()->PrintItemList();
-
-	//	monster2->removeToDroptable("Health_Potion_Common" , 1);
-	//	monster2->addToDroptable("Health_Potion_Rare" , 1);
-	for ( int i = 0; i < 10; i++ ) {
-		std::cout << monster2->getDropGold() << std::endl;
-		std::cout << monster3->getDropGold() << "\n" << std::endl;
-	}
-
-	for ( int i = 0; i < 10; i++ ) {
-		if ( monster2->getDropItem().GetItem() != nullptr ) {
-			std::cout << monster2->getDropItem().GetItem()->GetName() << std::endl;
-		}
-		if ( monster3->getDropItem().GetItem() != nullptr ) {
-			std::cout << monster3->getDropItem().GetItem()->GetName() << "\n" << std::endl;
-		}
-	}
-}
+//	BossMonster* monster3 = new BossMonster();
+//	monster3->getDropTableFromFile();
+//	monster3->getDroptable()->PrintItemList();
+//
+//	//	monster2->removeToDroptable("Health_Potion_Common" , 1);
+//	//	monster2->addToDroptable("Health_Potion_Rare" , 1);
+//	for ( int i = 0; i < 10; i++ ) {
+//		std::cout << monster2->getDropGold() << std::endl;
+//		std::cout << monster3->getDropGold() << "\n" << std::endl;
+//	}
+//
+//	for ( int i = 0; i < 10; i++ ) {
+//		if ( monster2->getDropItem().GetItem() != nullptr ) {
+//			std::cout << monster2->getDropItem().GetItem()->GetName() << std::endl;
+//		}
+//		if ( monster3->getDropItem().GetItem() != nullptr ) {
+//			std::cout << monster3->getDropItem().GetItem()->GetName() << "\n" << std::endl;
+//		}
+//	}
+//}
 //
 //std::cout << monster2->getDropItem().GetItem()->GetName() << std::endl;
 //	if ( monster2->getDropItem().GetItem() != nullptr ) {
